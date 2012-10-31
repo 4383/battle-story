8ee717d70b134e663a0a0b3d30ac68879e61c890
Hervé BERAUD <herveberaud.pro@gmail.com>
2012-10-31 17:21:18 +0100

    [EVO] Update for menu and other

--- source/main.py
+++ source/main.py
@@ -13,18 +13,10 @@ try:
     from cat import Cat
     from jukebox import Jukebox
     from const import LOG_FILE
+    from const import FPS_FREQUENCY
+    import menu
 except ImportError as e:
     print(e)
-
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-
-def quit_game():
-    logging.info('Pygame shutdown please wait...')
-    pygame.quit()
-    logging.info('Byby')
-    sys.exit(0)
-
 
 def main():
     """
@@ -36,30 +28,18 @@ def main():
     logging.info('Loading pygame...')
     pygame.init()
     logging.info('Initializing pygame...')
-    fps_frequency = 30
-    fps_clock = pygame.time.Clock()
-    logging.info('FPS frequency : %s' % fps_frequency)
+    logging.info('FPS frequency : %s' % FPS_FREQUENCY)
     window = pygame.display.set_mode((400, 300), 0, 32)
     pygame.display.set_caption('battle-story')
     cat = Cat(window)
     jukebox = Jukebox()
-    jukebox.play()
     logging.info('Running game')
 
     # A dégager
     start = 60
 
     while True:
-        window.fill(BLACK)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-              jukebox.stop()
-              quit_game()
-
-        cat.move()
-
-        pygame.display.update()
-        fps_clock.tick(fps_frequency)
+        menu.main_menu(window)
 
 if __name__ == '__main__':
     main()
