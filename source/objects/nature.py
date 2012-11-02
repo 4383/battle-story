7344899a4f04f9cb397de2b3d4689f691177432f
Hervé BERAUD <herveberaud.pro@gmail.com>
2012-11-02 16:56:45 +0100

    [EVO] Add object type cloud, code, animation, image

---
+++ source/objects/nature.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+author:Hervé BERAUD
+filename:nature.py
+"""
+try:
+    import sys
+    import pygame
+    from pygame.locals import *
+    import core
+except ImportError as e:
+    print(e)
+
+class Cloud(pygame.sprite.Sprite):
+    """
+    Nature element for background showing a simple cloud
+    """
+
+    def __init__(self, altitude, move=2):
+        """
+        """
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = core.load_image('cloud.png', -1)
+        self.altitude = altitude
+        self.rect.topleft = 0, altitude
+        self.move = move
+        self.altitude = altitude
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+
+    def update(self):
+        """
+        """
+        newpos = self.rect.move((self.move, 0))
+        if not self.area.contains(newpos):
+            if self.rect.left > self.area.right:
+                self.rect.topleft = 0, self.altitude
+                newpos = self.rect.move((self.move, 0))
+        self.rect = newpos
