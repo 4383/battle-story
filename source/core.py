3d516e3eb8cbaf4b6e67be5db8f6986eb4366504
Hervé BERAUD <herveberaud.pro@gmail.com>
2012-11-02 16:58:09 +0100

    [EVO] Add core function loading images for unique and multiple sprite

--- source/core.py
+++ source/core.py
@@ -5,10 +5,13 @@ author:Hervé BERAUD
 filename:core.py
 """
 try:
+    import os
     import sys
     import logging
     import pygame
+    from pygame.locals import *
     from jukebox import Jukebox
+    from const import RESOURCES_PATH_IMG
 except ImportError as e:
     print(e)
 
@@ -18,9 +21,44 @@ def quit_game():
     """
     logging.info('Pygame shutdown please wait...')
     jukebox = Jukebox()
+    jukebox.stop()
     jukebox.quit()
     pygame.quit()
     logging.info('Byby')
     sys.exit(0)
 
+def load_image(name, colorkey=None, alpha=False):
+    """
+    load a simple picture
+    """
+    fullname = '{0}{1}' . format(RESOURCES_PATH_IMG, name)
+    try:
+        if not alpha:
+            image = pygame.image.load(fullname)
+        else:
+            image = pygame.image.load(fullname).convert_alpha()
+    except pygame.error, message:
+        print "image not find :", name
+        raise SystemExit, message
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    if not alpha:
+        return image, image.get_rect()
+    else:
+        return image, image.get_size()
 
+def load_sliced_sprites(self, w, h, filename):
+    """
+    Load a composed picture from sprite file.
+    Using for creating animation
+    """
+    images = []
+    master_image, master_size = load_image(filename)
+
+    master_width, master_height = master_size
+    for i in xrange(int(master_width/w)):
+        images.append(master_image.subsurface((i*w,0,w,h)))
+    return images
