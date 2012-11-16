dc6a1b6cfc4a15bb7ddf0a1c57b25a2defa91e31
Hervé BERAUD <herveberaud.pro@gmail.com>
2012-11-16 15:54:23 +0100

    [EVO] Adding class Game for engine game generator

--- source/engine/game.py
+++ source/engine/game.py
@@ -11,13 +11,14 @@ try:
     import datetime
     import pygame
     from pygame.locals import *
-    from engine.jukebox import Jukebox
     from const import RESOURCES_PATH_IMG_MAPS
     from const import CONFIG_LEVEL_PATH
     from const import LOG_FILE
     from const import FPS_FREQUENCY
+    from const import FPS_CLOCK
     from const import WINDOW_WIDTH
     from const import WINDOW_HEIGHT
+    from objects.decor import Block
     import menu
 except ImportError as e:
     print(e)
@@ -37,7 +38,8 @@ class Game():
             raise Exception('Configuration level file is not defined')
         self.config_file = '{0}{1}.ini' . format(CONFIG_LEVEL_PATH, level_conf_name)
         self.load_conf()
-        level = Level(self.conf_level)
+        self.level = Level(self.conf_level)
+        self.level.render()
 
     def load_conf(self):
         """
@@ -52,6 +54,13 @@ class Game():
         #self.map_pattern = config.get('MAPPING', 'PATTERN')
         #self.map_render = config.get('MAPPING', 'RENDER')
 
+    def play(self, window):
+        again = True
+        while again:
+            self.level.get_group_map().update()
+            self.level.get_group_map().draw(window)
+            pygame.display.flip()
+            FPS_CLOCK.tick(FPS_FREQUENCY)
 
 class Level():
     """
@@ -65,10 +74,9 @@ class Level():
         if type(conf) is not 'dict':
             self.conf = dict(conf)
 
-        print(self.conf['mapper'])
-        print(self.conf['mapper_widht'])
-        print(self.conf['mapper_height'])
-
+        self._group_solid_block = pygame.sprite.RenderUpdates()
+        self._group_non_solid_block = pygame.sprite.RenderUpdates()
+        self._group_map = pygame.sprite.RenderUpdates()
 
         self._set_mapper(
             self.conf['mapper'],
@@ -86,6 +94,12 @@ class Level():
             self.conf['render'],
             self.conf['render_width'],
             self.conf['render_height']
+        )
+
+        self._set_pattern_solid_block(
+            self.conf['pattern_solid_block'],
+            self.conf['pattern_solid_block_width'],
+            self.conf['pattern_solid_block_height']
         )
 
     def _check_conf():
@@ -115,26 +129,90 @@ class Level():
             int_block_width,
             int_block_height
         )
-        print(self._pattern)
+
+        tmp_pattern = []
+        for row in self._pattern:
+            for column in row:
+                tmp_pattern.append(column)
+
+        self._pattern = tmp_pattern
+
+    def _set_pattern_solid_block(self, str_picture, int_block_width, int_block_height):
+        """
+        """
+        self._pattern_solid_block = self._loader(
+            str_picture,
+            int_block_width,
+            int_block_height
+        )
+
+        tmp_pattern = []
+        for row in self._pattern_solid_block:
+            for column in row:
+                tmp_pattern.append(column)
+
+        self._pattern_solid_block = tmp_pattern
 
     def _set_render(self, str_picture, int_block_width, int_block_height):
         """
         """
-        self._render, self.render_rect = self._loader(
+        self._render, self._render_rect = self._loader(
             str_picture,
             int_block_width,
             int_block_height,
             False
         )
-        print(self._render)
 
-    def make(self):
+    def render(self):
         """
         """
+        mapping = []
+        posy = 0
+        for index_row, row in enumerate(self._mapper):
+            mapping.append([])
+            posx = 0
+            for column in row:
+                index_pattern = self._pattern.index(column)
+                image = self._render[index_pattern]
+                block = Block(image, (posx, posy))
+                mapping[index_row].append(block)
+                self._group_map.add(block)
+                self.set_block_solidity(column, block)
+                posx += int(self.conf['render_width'])
+            posy += int(self.conf['render_height'])
+        return mapping, self._group_map
+
+    def get_group_map(self):
+        """
+        """
+        return self._group_map
+
+    def get_solid_block(self):
+        """
+        """
+        return self._group_solid_block
+
+    def get_non_solid_block(self):
+        """
+        """
+        return self._group_non_solid_block
+
+    def set_block_solidity(self, pattern, block):
+        """
+        Dispatch block parameters into the differents groups.
+        If the pattern index is in pattern solid block list this is a solid block
+        else is not a solid block
+        pygame.Surface pattern is the block pattern to compare with pattern solid
+        pygame.sprite.Sprite block is the sprite to insert into group
+        """
+        if pattern in self._pattern_solid_block:
+            self._group_solid_block.add(block)
+        else:
+            self._group_non_solid_block.add(block)
 
     def _loader(self, str_picture, int_width, int_height, bool_abstract=True):
         """
-        Load an image and extract and create mapping matrice contening
+        Load an image or extract and create mapping matrice contening
         list of color ordered or just load
         and extract image and return ordered array contening picture
         """
@@ -176,7 +254,7 @@ class Level():
         else:
             return images, rects
 
-    def _map_to_render(self):
+    def get_solid_block(self):
         """
         """
 
